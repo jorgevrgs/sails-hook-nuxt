88112ac088471e2e3f62fc6e5e5d5ff4881fe7a0
Jorge Vargas <jorgevargasla@gmail.com>
2021-07-02 23:10:52 -0500

    test: create example actions ssr

---
+++ example/client/store/actions.js
@@ -0,0 +1,16 @@
+export default {
+  async nuxtServerInit({ commit }, { req, beforeNuxtRender }) {
+    if (process.server) {
+      beforeNuxtRender(({ nuxtState }) => {
+        // Get csrf token
+        const csrf = req.csrfToken()
+
+        // Available as window.__NUXT__.csrf
+        nuxtState.csrf = csrf
+
+        // Require define mutations
+        commit('SET_CSRF', csrf)
+      })
+    }
+  },
+}
